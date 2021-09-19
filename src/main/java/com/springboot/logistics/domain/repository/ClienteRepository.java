d7cb29f35ef1e1565bc67a8ad8785ee08f980eb5
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2021-09-19 15:26:01 -0300

    Adicionado CRUD de Clientes

---
+++ src/main/java/com/springboot/logistics/domain/repository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package com.springboot.logistics.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.logistics.domain.model.Cliente;
+
+@Repository // antação que define que a interface (ClienteRepository neste caso) é um
+			// componente do Spring porém com uma semântica muito bem definida, ou seja, é
+			// um repositório. Um repositório é o que gerencia uma entidade, neste caso é a entidade Cliente
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+
+}
