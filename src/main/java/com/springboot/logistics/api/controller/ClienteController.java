7160c1d97dba415fc06085ebb974c6d83da69dd7
Leandro Eduardo <leandro.silva.eduardo1@gmail.com>
2021-09-24 05:05:47 -0300

    Finaliza implementação da camada Service para Clientes

--- src/main/java/com/springboot/logistics/api/controller/ClienteController.java
+++ src/main/java/com/springboot/logistics/api/controller/ClienteController.java
@@ -1,14 +1,14 @@
 package com.springboot.logistics.api.controller;
 
+import java.net.URI;
+
 import javax.validation.Valid;
 
 import com.springboot.logistics.domain.model.Cliente;
-import com.springboot.logistics.domain.repository.ClienteRepository;
 import com.springboot.logistics.domain.service.ClienteService;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.AllArgsConstructor;
 
@@ -27,45 +27,35 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/clientes")
 public class ClienteController {
 
-	private ClienteRepository clienteRepository;
 	private ClienteService clienteService;
 
 	@GetMapping
 	public Page<Cliente> listar(Pageable pageable) {
 		return clienteService.listar(pageable);
 	}
-		
+
 	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
-		return ResponseEntity.status(HttpStatus.OK).body(clienteService.buscar(clienteId));
+		return ResponseEntity.ok(clienteService.buscar(clienteId));
 	}
-	
+
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
-		return clienteService.salvar(cliente);
+	public ResponseEntity<Cliente> incluir(@Valid @RequestBody Cliente cliente) {
+		clienteService.incluir(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{clienteId}").buildAndExpand(cliente.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(cliente);
 	}
-	
+
 	@PutMapping("/{clienteId}")
 	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente) {
-		if (!clienteRepository.existsById(clienteId)) {
-			return ResponseEntity.notFound().build();
-		}
-		
-		cliente.setId(clienteId);
-		cliente = clienteService.salvar(cliente);
-		
-		return ResponseEntity.ok(cliente);
+		Cliente clienteSalvo = clienteService.atualizar(clienteId, cliente);
+		return ResponseEntity.ok(clienteSalvo);
 	}
-	
+
 	@DeleteMapping("/{clienteId}")
 	public ResponseEntity<Void> excluir(@PathVariable Long clienteId) {
-		if (!clienteRepository.existsById(clienteId)) {
-			return ResponseEntity.notFound().build();
-		}
-		
 		clienteService.excluir(clienteId);
-		
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/springboot/logistics/domain/service/ClienteService.java
+++ src/main/java/com/springboot/logistics/domain/service/ClienteService.java
@@ -5,6 +5,7 @@ import com.springboot.logistics.domain.repository.ClienteRepository;
 import com.springboot.logistics.domain.service.exceptions.ClienteExistenteException;
 import com.springboot.logistics.domain.service.exceptions.ClienteNaoEncontradoException;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -25,8 +26,8 @@ public class ClienteService {
 	}
 
 	public Cliente buscar(Long clienteId) {
-		return clienteRepository.findById(clienteId)
-				.orElseThrow(() -> new ClienteNaoEncontradoException("Cliente não encontrado"));
+		return clienteRepository.findById(clienteId).orElseThrow(
+				() -> new ClienteNaoEncontradoException("Cliente com o ID " + clienteId + " não encontrado"));
 	}
 
 	@Transactional // declara que este método deve ser executado dentro de uma transação, ou seja,
@@ -34,21 +35,27 @@ public class ClienteService {
 					// operações dentro dessa transação que estão sendo feitas no banco de dados são
 					// descartadas (ou tudo é executado ou nada é executado quando persistimos dados
 					// no banco)
-	public Cliente salvar(Cliente cliente) {
-		boolean emailExistente = clienteRepository.findByEmail(cliente.getEmail())
-				.stream()
+	public Cliente incluir(Cliente cliente) {
+		boolean emailExistente = clienteRepository.findByEmail(cliente.getEmail()).stream()
 				.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
-		
 		if (emailExistente) {
 			throw new ClienteExistenteException("Já existe um cliente cadastrado com este e-mail");
 		}
-		
 		return clienteRepository.save(cliente);
 	}
-	
+
 	@Transactional
-	public void excluir(Long id) {
-		clienteRepository.deleteById(id);
+	public Cliente atualizar(Long clienteId, Cliente cliente) {
+		Cliente clienteSalvo = buscar(clienteId);
+		BeanUtils.copyProperties(cliente, clienteSalvo);
+		clienteSalvo.setId(clienteId);
+		return clienteRepository.save(clienteSalvo);
+	}
+
+	@Transactional
+	public void excluir(Long clienteId) {
+		Cliente clienteSalvo = buscar(clienteId);
+		clienteRepository.deleteById(clienteSalvo.getId());
 	}
 
 }
